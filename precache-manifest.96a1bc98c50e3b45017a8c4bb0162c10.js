ed0ddce1f829ea158cdefbeb70f73b32fa01cb00
bruno <bruno.cami@hotmail.com>
2022-04-07 20:28:47 -0300

    Updates

---
+++ precache-manifest.96a1bc98c50e3b45017a8c4bb0162c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74899c50d25c5a53e0fe95b59a54e7de",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "26f755d8b13e2e717541",
+    "url": "/quiz/static/css/main.50566d9f.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/quiz/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f755d8b13e2e717541",
+    "url": "/quiz/static/js/main.499827b7.chunk.js"
+  },
+  {
+    "revision": "dd19b6e4ede99b84197b",
+    "url": "/quiz/static/js/runtime-main.7a6c2dfa.js"
+  }
+]);
